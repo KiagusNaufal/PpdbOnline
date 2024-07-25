9a17e0bf34ebfce6374dc950f5812800d1e1ad11
“Raehan <attayaraehan@gmail.com>
2024-07-25 08:20:35 +0700

    membuat form data diri siswa

--- database/migrations/2024_07_25_002728_create_students_table.php
+++ database/migrations/2024_07_25_002728_create_students_table.php
@@ -1,27 +1,40 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateStudentTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('student', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_lengkap');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->text('alamat');
+            $table->string('no_kk');
+            $table->string('nik')->unique();
+            $table->string('nisn')->unique();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('student');
     }
-};
+}
